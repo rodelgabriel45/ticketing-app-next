58c795d54e0ce436103a9ef1eacc58999851f363
rodelgabriel45@gmail.com <rodelgabriel45@gmail.com>
2024-06-19 19:27:40 +0800

    add next-auth api endpoint

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,62 @@
+import { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import bcryptjs from 'bcryptjs';
+
+import prisma from '@/prisma/db';
+
+const options: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: 'password',
+      name: 'Username and Password',
+      credentials: {
+        username: {
+          label: 'Username',
+          type: 'text',
+          placeholder: 'Username...',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'Password...',
+        },
+      },
+      authorize: async (credentials) => {
+        const user = await prisma.user.findUnique({
+          where: { username: credentials!.username },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        const match = await bcryptjs.compare(
+          credentials!.password,
+          user.password
+        );
+
+        if (!match) {
+          return null;
+        }
+
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, user }) {
+      if (account) {
+        token.role = user.role;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      if (session.user) {
+        session.user.role = token.role || 'USER';
+      }
+      return session;
+    },
+  },
+};
+
+export default options;
