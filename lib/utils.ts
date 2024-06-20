e2fb9fa92fc384f2499d42888371f3bcf1aa31ab
rodelgabriel45@gmail.com <rodelgabriel45@gmail.com>
2024-06-20 23:02:29 +0800

    fix utils date options error

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,15 +7,13 @@ export function cn(...inputs: ClassValue[]) {
 
 export const formatDate = (date: Date) => {
   {
-    const options = {
+    return new Intl.DateTimeFormat('en-US', {
       year: '2-digit',
       month: '2-digit',
       day: '2-digit',
       hour: 'numeric',
       minute: '2-digit',
       hour12: true,
-    };
-
-    return new Intl.DateTimeFormat('en-US', options).format(date);
+    }).format(date);
   }
 };
